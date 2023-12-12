eea4c86d66713f1d1bc79e63e129463047bcef4c
Adam Prchal <prchaladam@outlook.cz>
2023-12-12 20:43:11 +0100

    allowed hosts

--- rezervobot/rezervobot/settings.py
+++ rezervobot/rezervobot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-n013^o@e2i66hdgpu^cs%p!@%y!opxi0ep#sv(u%ld&268&fa&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
